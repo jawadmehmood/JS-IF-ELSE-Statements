38003e1622fb7c16360e7164a3c3813ef9f876b1
jawadmehmood <jawadam5678@gmail.com>
2024-07-07 05:01:03 +0500

    4 assignments completed

---
+++ app.js
@@ -0,0 +1,52 @@
+// Assignment 1
+
+var inputnum1 = +prompt('Input a number to check whether it is divisible by 3 or not');
+
+if(inputnum1 % 3 === 0){
+    console.log('The number is divisible by 3 and the answer is ' + inputnum1 / 3);
+}
+else{
+    console.log('Number is not divisible by 3.');
+}
+
+// Assignment 2
+
+var inputnum2 = +prompt('Input a number to check whether it is an even number or odd number');
+
+if(inputnum2 % 2 === 0){
+    console.log('It is an Even number');
+}
+else {
+    console.log('It is an Odd number');
+}
+
+// Assignment 3
+
+var age = +prompt('Enter your age');
+
+if(age > 18){
+    console.log('Old enough');
+}
+else{
+    console.log('Too young');
+}
+
+// Assignment 4
+
+var username = prompt('Enter your name');
+
+if(username === "jawad"){
+    alert('Welcome to my website ' + username);
+}
+else{
+
+}
+
+// Assignment 5
+
+
+// Assignment 6 
+// Assignment 7 
+// Assignment 8 
+// Assignment 9 
+// Assignment 10 
